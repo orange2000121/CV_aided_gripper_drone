14ddf039d1ed1387884f3368a125ebb11a78c512
yuan-0816 <wj582693@gmail.com>
2023-07-25 20:30:58 +0800

    delete virtual sticks Buttons and replace them with Switch id is "SwitchVirtualStick"

---
+++ ActivationAndBindingDemo/app/src/main/java/com/dji/activationDemo/yuan.java
@@ -0,0 +1,14 @@
+package com.dji.activationDemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class yuan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yuan);
+    }
+}
